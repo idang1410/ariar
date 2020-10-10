b5220091a1d918d8e329cdaebc25ee7b08c08f00
Idan <idang1410@gmail.com>
2020-10-10 18:02:05 +0300

    Command line arguemnt parsed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let params = get_params();
+    println!("the params {}", params);
 }
+
+/*
+ * handle the command line arguments
+ */
+fn get_params() -> String {
+    let matches = App::new("ariar")
+        .version("0.1")
+        .about("download manager written in rust")
+        .author("idang1410");
+    let param = Arg::with_name("file")
+        .long("file")
+        .short("f")
+        .value_name("FILE")
+        .help("torrent file")
+        .required(true);
+
+    let matches = matches.arg(param);
+    let matches = matches.get_matches();
+    String::from(matches.value_of("file").unwrap().clone())
+}
+
